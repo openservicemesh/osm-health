bad25301954eecdd196beb9ed2f1d881805a4774
Sanya Kochhar <kochhars@microsoft.com>
2021-07-21 17:20:21 -0400

    feat(connectivity): add check for envoy sidecar
    Signed-off-by: Sanya Kochhar <kochhars@microsoft.com>

--- pkg/connectivity/pod-to-pod.go
+++ pkg/connectivity/pod-to-pod.go
@@ -3,6 +3,7 @@ package connectivity
 import (
 	"fmt"
 	v1 "k8s.io/api/core/v1"
+	"strings"
 
 	"github.com/openservicemesh/osm-health/pkg/common"
 )
@@ -12,6 +13,8 @@ func PodToPod(fromPod *v1.Pod, toPod *v1.Pod) common.Result {
 	log.Info().Msgf("Testing connectivity from %s/%s to %s/%s", fromPod.Namespace, fromPod.Name, toPod.Namespace, toPod.Name)
 
 	// TODO: actually test connectivity
+	fmt.Printf("Pod %s has an envoy sidecar: %t\n", fromPod.Name, hasEnvoySideCar(fromPod))
+	fmt.Printf("Pod %s has an envoy sidecar: %t\n", fromPod.Name, hasEnvoySideCar(toPod))
 
 	return common.Result{
 		SMIPolicy: common.SMIPolicy{
@@ -21,4 +24,13 @@ func PodToPod(fromPod *v1.Pod, toPod *v1.Pod) common.Result {
 		},
 		Successful: false,
 	}
+}
+
+func hasEnvoySideCar(pod *v1.Pod) bool {
+	for _, container := range pod.Spec.Containers {
+		if strings.Contains(container.Image, "envoy") {
+			return true
+		}
+	}
+	return false
 }
