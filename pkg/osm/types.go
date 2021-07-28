af37a5fd583f06faccabb4c124616fda7afc6914
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-28 08:37:46 -0700

    Adding a mapping from OSM version to supported Ingress version

---
+++ pkg/osm/types.go
@@ -0,0 +1,7 @@
+package osm
+
+// ControllerVersion is a string type alias for the OSM version.
+type ControllerVersion string
+
+// IngressVersion is a string type alias for the Ingress version supported.
+type IngressVersion string
