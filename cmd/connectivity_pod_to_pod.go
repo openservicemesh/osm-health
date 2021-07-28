31ea375e5754922aea211b93970ab59430a64f2d
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-28 07:59:39 -0700

    Use the new GetKubeClient()
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- cmd/connectivity_pod_to_pod.go
+++ cmd/connectivity_pod_to_pod.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/kubernetes"
 
 	"github.com/openservicemesh/osm-health/pkg/connectivity"
 	"github.com/openservicemesh/osm-health/pkg/kuberneteshelper"
@@ -20,26 +19,16 @@ Example:
 `
 
 func newConnectivityPodToPodCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "pod-to-pod SOURCE_POD DESTINATION_POD",
-		Short: "Checks connectivity between Kubernetes pods",
-		Long:  connectivityPodToPodDesc,
-		Args:  cobra.ExactArgs(2),
+	return &cobra.Command{
+		Use:     "pod-to-pod SOURCE_POD DESTINATION_POD",
+		Short:   "Checks connectivity between Kubernetes pods",
+		Example: connectivityPodToPodExample,
+		Long:    connectivityPodToPodDesc,
+		Args:    cobra.ExactArgs(2),
 		RunE: func(_ *cobra.Command, args []string) error {
 			if len(args) < 2 {
 				return errors.Errorf("provide both SOURCE_POD and DESTINATION_POD")
 			}
-
-			config, err := settings.RESTClientGetter().ToRESTConfig()
-			if err != nil {
-				return errors.Errorf("Error fetching kubeconfig: %s", err)
-			}
-
-			clientset, err := kubernetes.NewForConfig(config)
-			if err != nil {
-				return errors.Errorf("Could not access Kubernetes cluster, check kubeconfig: %s", err)
-			}
-			clientSet := clientset
 
 			fromPod, err := kuberneteshelper.PodFromString(args[0])
 			if err != nil {
@@ -51,10 +40,8 @@ func newConnectivityPodToPodCmd() *cobra.Command {
 				return errors.New("invalid DESTINATION_POD")
 			}
 
-			connectivity.PodToPod(clientSet, fromPod, toPod)
+			connectivity.PodToPod(fromPod, toPod)
 			return nil
 		},
-		Example: connectivityPodToPodExample,
 	}
-	return cmd
 }
