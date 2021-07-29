06ea6dad6d107765719e486d2f478bf027afeb0f
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-29 08:55:54 -0700

    Fix types

--- pkg/envoy/lds.go
+++ pkg/envoy/lds.go
@@ -3,13 +3,14 @@ package envoy
 import (
 	"errors"
 
+	v1 "k8s.io/api/core/v1"
+
 	"github.com/openservicemesh/osm-health/pkg/common"
-	k8s "github.com/openservicemesh/osm-health/pkg/kubernetes"
 )
 
 // HasListener implements common.Runnable
 type HasListener struct {
-	k8s.Pod
+	*v1.Pod
 }
 
 // Run implements common.Runnable
@@ -30,7 +31,7 @@ func (l HasListener) Info() string {
 }
 
 // NewHasListener creates a new common.Runnable, which checks whether the given Pod has an Envoy with properly configured listener.
-func NewHasListener(pod k8s.Pod) common.Runnable {
+func NewHasListener(pod *v1.Pod) common.Runnable {
 	return HasListener{
 		Pod: pod,
 	}
