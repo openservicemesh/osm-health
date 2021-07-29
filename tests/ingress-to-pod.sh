4a3609423ad59d13d970e19757a9ec094a2a16a6
Delyan Raychev <delyan.raychev@microsoft.com>
2021-07-28 17:14:39 -0700

    tests: Wait for pods
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- tests/ingress-to-pod.sh
+++ tests/ingress-to-pod.sh
@@ -6,6 +6,10 @@ kubectl create namespace bookstore
 ./osm namespace add bookstore
 
 
+while [[ $(kubectl get pods -n "bookstore" -l "app=bookstore" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+    echo "waiting for pod" && sleep 1
+done
+
 POD=$(kubectl get pod -n bookbuyer --selector app=bookbuyer --no-headers | awk '{print $1}')
 
 ./bin/osm-health ingress to-pod "bookbuyer/${POD}"
--- tests/pod-to-pod.sh
+++ tests/pod-to-pod.sh
@@ -5,6 +5,11 @@
 kubectl create namespace bookstore
 ./osm namespace add bookstore
 
+for x in bookbuyer bookstore; do
+    while [[ $(kubectl get pods -n "$x" -l "app=${x}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+        echo "waiting for pod" && sleep 1
+    done
+done
 
 POD1=$(kubectl get pod -n bookbuyer --selector app=bookbuyer --no-headers | awk '{print $1}')
 POD2=$(kubectl get pod -n bookstore --selector app=bookstore --no-headers | awk '{print $1}')
