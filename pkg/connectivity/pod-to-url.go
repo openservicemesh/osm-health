95245bad55fda8d0b7a2aa5f720069c1f9283d53
Delyan Raychev <delyan.raychev@microsoft.com>
2021-08-04 14:04:52 -0700

    Clean up unused structs
    Signed-off-by: Delyan Raychev <delyan.raychev@microsoft.com>

--- pkg/connectivity/pod-to-url.go
+++ pkg/connectivity/pod-to-url.go
@@ -9,19 +9,8 @@ import (
 )
 
 // PodToURL tests the connectivity between a source pod and destination url.
-func PodToURL(fromPod *v1.Pod, destinationURL *url.URL) common.Result {
+func PodToURL(fromPod *v1.Pod, destinationURL *url.URL) {
 	log.Info().Msgf("Testing connectivity from %s/%s to %s", fromPod.Namespace, fromPod.Name, destinationURL)
-
 	outcomes := common.Run()
-
 	common.Print(outcomes...)
-
-	return common.Result{
-		SMIPolicy: common.SMIPolicy{
-			HasPolicy:                  false,
-			ValidPolicy:                false,
-			SourceToDestinationAllowed: false,
-		},
-		Successful: false,
-	}
 }
