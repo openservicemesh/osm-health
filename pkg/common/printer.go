f0cb18ab057c3dd83baf75b26580d36b92876310
Sanya Kochhar <kochhars@microsoft.com>
2021-08-04 18:14:47 -0400

    fmt(printer): print check results as a table
    Signed-off-by: Sanya Kochhar <kochhars@microsoft.com>

--- pkg/common/printer.go
+++ pkg/common/printer.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/fatih/color"
 )
@@ -10,6 +12,10 @@ import (
 func Print(outcomes ...Outcome) {
 	issuesCount := 0
 	foundIssues := false
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 8, 8, 0, ' ', 0)
+
+	defer func() { _ = w.Flush() }()
 	for idx, issue := range outcomes {
 		foundIssues = foundIssues && issue.Error != nil
 		errString := color.GreenString("OK")
@@ -17,7 +23,7 @@ func Print(outcomes ...Outcome) {
 			errString = fmt.Sprintf("%s %s", color.RedString("FAIL:"), color.RedString(issue.Error.Error()))
 			issuesCount = issuesCount + 1
 		}
-		fmt.Printf("%d  %s  -- %s\n", idx+1, issue.RunnableInfo, errString)
+		fmt.Fprintf(w, "%d   %s\t%s\t\n", idx+1, issue.RunnableInfo, errString)
 	}
 
 	if foundIssues {
