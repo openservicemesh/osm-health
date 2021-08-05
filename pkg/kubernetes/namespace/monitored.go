9634c6cf695ef20160283abec35a3b6d1d900870
Sanya Kochhar <kochhars@microsoft.com>
2021-08-05 14:55:20 -0400

    check(monitored): are namespaces monitored by same mesh
    Signed-off-by: Sanya Kochhar <kochhars@microsoft.com>

--- pkg/kubernetes/namespace/monitored.go
+++ pkg/kubernetes/namespace/monitored.go
@@ -59,3 +59,60 @@ func (check MonitoredCheck) Suggestion() string {
 func (check MonitoredCheck) FixIt() error {
 	panic("implement me")
 }
+
+// Verify interface compliance
+var _ common.Runnable = (*NamespacesInSameMeshCheck)(nil)
+
+// NamespacesInSameMeshCheck implements common.Runnable
+type NamespacesInSameMeshCheck struct {
+	client     kubernetes.Interface
+	namespaceA string
+	namespaceB string
+}
+
+// AreNamespacesInSameMesh checks whether two pods are in the same mesh
+func AreNamespacesInSameMesh(client kubernetes.Interface, namespaceA string, namespaceB string) NamespacesInSameMeshCheck {
+	return NamespacesInSameMeshCheck{
+		client:     client,
+		namespaceA: namespaceA,
+		namespaceB: namespaceB,
+	}
+}
+
+// Info implements common.Runnable
+func (check NamespacesInSameMeshCheck) Info() string {
+	return fmt.Sprintf("Checking whether namespace %s and namespace %s are monitored by the same mesh", check.namespaceA, check.namespaceB)
+}
+
+// Run implements common.Runnable
+func (check NamespacesInSameMeshCheck) Run() error {
+	labelsA, err := getLabels(check.client, check.namespaceA)
+	if err != nil {
+		return err
+	}
+	meshNameA, labelExistsA := labelsA[constants.OSMKubeResourceMonitorAnnotation]
+
+	labelsB, err := getLabels(check.client, check.namespaceB)
+	if err != nil {
+		return err
+	}
+
+	meshNameB, labelExistsB := labelsB[constants.OSMKubeResourceMonitorAnnotation]
+	if !labelExistsA || !labelExistsB {
+		return ErrNotMonitoredByOSMController
+	}
+	if meshNameA != meshNameB {
+		return ErrNamespacesNotInSameMesh
+	}
+	return nil
+}
+
+// Suggestion implements common.Runnable
+func (check NamespacesInSameMeshCheck) Suggestion() string {
+	return fmt.Sprintf("Verify which mesh each namespace is monitored by. Try: \"kubectl get namespace -n %s -o json | jq '.items[0].metadata.labels'\"", check.namespaceA)
+}
+
+// FixIt implements common.Runnable
+func (check NamespacesInSameMeshCheck) FixIt() error {
+	panic("implement me")
+}
