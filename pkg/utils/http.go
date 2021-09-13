26af3821c7face5bafd3b09c4d02193ee2b3e51c
Johnson Shi <Johnson.Shi@microsoft.com>
2021-09-13 05:38:59 -0700

    check(controller): Check controller http server health probes.
    Checks controller http server health probes for
    the health of the osm-controller pods.
    
    Signed-off-by: Johnson Shi <Johnson.Shi@microsoft.com>

--- pkg/utils/http.go
+++ pkg/utils/http.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+<<<<<<< HEAD
+=======
+	"io/ioutil"
+>>>>>>> aece626 (check(controller): Check controller http server health probes.)
 	"net/http"
 
 	"github.com/pkg/errors"
@@ -19,4 +23,24 @@ func IsHTTPResponseCodeEquals(url string, statusCode int) error {
 	}
 
 	return nil
+}
+
+// GetResponseBody returns the respo
+func GetResponseBody(url string) (string, error) {
+	// #nosec G107: Potential HTTP request made with variable url
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", errors.Errorf("error fetching (GET) url %s: %s", url, err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.Errorf("url returned http status code: %d", resp.StatusCode)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Errorf("error rendering HTTP response: %s", err)
+	}
+
+	return string(respBody), nil
 }
