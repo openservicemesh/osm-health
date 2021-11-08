92b96e475077ee83df442be1ee363013751e7a33
Thomas Stringer <thstring@microsoft.com>
2021-11-08 15:51:38 -0500

    add tcp_proxy for envoy config
    Signed-off-by: Thomas Stringer <thstring@microsoft.com>

--- pkg/envoy/parse.go
+++ pkg/envoy/parse.go
@@ -13,6 +13,7 @@ import (
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/wasm/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/rbac/v3"
+	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/tcp_proxy/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/upstreams/http/v3"
 
 	"google.golang.org/protobuf/encoding/protojson"
