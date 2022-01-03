9e78a83b0cb7cb650763c59026053b75dfdd3002
Johnson Shi <Johnson.Shi@microsoft.com>
2022-01-03 14:23:15 -0800

    feat(cli): show error messages for wrong arg count
    Shows meaningful, verbose, and friendly error messages when
    incorrect number of args are presented to the command.
    
    Resolves #116.
    
    Signed-off-by: Johnson Shi <Johnson.Shi@microsoft.com>

--- cmd/ingress_to_pod.go
+++ cmd/ingress_to_pod.go
@@ -4,23 +4,21 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
+	"github.com/openservicemesh/osm-health/pkg/cli"
 	"github.com/openservicemesh/osm-health/pkg/ingress"
 	"github.com/openservicemesh/osm-health/pkg/kubernetes/pod"
 )
 
-const ingressToPodExample = `$ osm-health ingress to-pod namespace-a/pod-a`
+const ingressToPodExample = `$ osm-health ingress to-pod destination-namespace/destination-pod`
 
 func newIngressToPodCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "to-pod DESTINATION_POD",
+		Use:     "to-pod destination-namespace/destination-pod",
 		Short:   "Checks ingress to a given Kubernetes pod",
 		Example: ingressToPodExample,
 		Long:    `Checks ingress to a given Kubernetes pod`,
-		Args:    cobra.ExactArgs(1),
+		Args:    cli.ExactArgsWithError(1, errors.New("requires 1 argument: destination-namespace/destination-pod")),
 		RunE: func(_ *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return errors.Errorf("missing DESTINATION_POD parameter")
-			}
 			log.Info().Msgf("Checking Ingress to Pod %s", args[0])
 
 			client, err := pod.GetKubeClient()
@@ -30,7 +28,7 @@ func newIngressToPodCmd() *cobra.Command {
 
 			dstPod, err := pod.FromString(args[0])
 			if err != nil {
-				return errors.New("invalid DESTINATION_POD")
+				return errors.New("invalid destination-namespace/destination-pod")
 			}
 
 			osmControlPlaneNamespace := settings.Namespace()
